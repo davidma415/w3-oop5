115a97ce2fb215c5a26f8e631fc9c3dd6ba3f1ec
David Ma <davidma415@gmail.com>
2018-07-27 00:01:59 -0400

    Finished all basic requirements of crop class

---
+++ crop.rb
@@ -0,0 +1,63 @@
+class Crop
+  @@all_crops = []
+  @@harvested = 0
+  attr_reader :type, :size
+
+  def initialize(type, size)
+    @type = type
+    @size = size
+  end
+
+  def self.create(type, size)
+    new_crop = self.new(type, size)
+    @@all_crops << new_crop
+  end
+
+  def self.all
+    return @@all_crops
+  end
+
+  def self.harvest
+    @@all_crops.each {|crop|
+      if crop.type.downcase == 'corn'
+        puts "Harvesting #{20*crop.size} food from #{crop.size} hectare #{crop.type} field"
+        @@harvested += (20*crop.size)
+      elsif crop.type.downcase == 'wheat'
+        puts "Harvesting #{30*crop.size} food from #{crop.size} hectare #{crop.type} field"
+        @@harvested += (30*crop.size)
+      end
+    }
+  end
+
+  def self.harvested
+    puts "The farm has harvested #{@@harvested} food so far"
+  end
+
+  def self.status
+    @@all_crops.each {|crop| puts "#{crop.type.capitalize} field is #{crop.size} hectares"}
+    self.harvested
+  end
+
+  def self.relax
+    corn_count = 0
+    wheat_count = 0
+    @@all_crops.each {|crop|
+      if crop.type.downcase == 'corn'
+        corn_count += crop.size
+      elsif crop.type.downcase == 'wheat'
+        wheat_count += crop.size
+      end
+    }
+    puts "#{corn_count} hectares of tall green stalks rustling in the breeze fill your horizon. The sun hangs low, casting an orange glow on a sea of #{wheat_count} hectares of wheat."
+  end
+end
+
+crop1 = Crop.create('corn', 50)
+crop2 = Crop.create('wheat', 40)
+crop3 = Crop.create('corn', 50)
+crop4 = Crop.create('wheat', 40)
+all = Crop.all
+
+Crop.harvest
+Crop.status
+Crop.relax
